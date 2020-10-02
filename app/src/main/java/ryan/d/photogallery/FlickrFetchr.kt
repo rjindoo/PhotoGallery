f843b5236e873437882dc28d74f356f646526a3b
rjindoo <platypus2k1@gmail.com>
2020-10-01 21:57:53 -0700

    Setup PhotoGallery to dynamically display photos as recyclerView updates

--- app/src/main/java/ryan/d/photogallery/FlickrFetchr.kt
+++ app/src/main/java/ryan/d/photogallery/FlickrFetchr.kt
@@ -1,8 +1,12 @@
 package ryan.d.photogallery
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.util.Log
+import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,6 +30,14 @@ class FlickrFetchr {
             .build()
 
         flickrApi = retrofit.create(FlickrApi::class.java)
+    }
+
+    @WorkerThread
+    fun fetchPhoto(url:String): Bitmap? {
+        val response: Response<ResponseBody> = flickrApi.fetchUrlBytes(url).execute()
+        val bitmap = response.body()?.byteStream()?.use(BitmapFactory::decodeStream)
+        Log.i(TAG, "Decoded bitmap=$bitmap from Response=$response")
+        return bitmap
     }
 
     fun fetchPhotos(): LiveData<List<GalleryItem>> {
--- app/src/main/java/ryan/d/photogallery/api/FlickrApi.kt
+++ app/src/main/java/ryan/d/photogallery/api/FlickrApi.kt
@@ -1,9 +1,12 @@
 package ryan.d.photogallery.api
 
+import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 interface FlickrApi {
+
 
     @GET("/" +
             "services/rest/?method=flickr.interestingness.getList" +
@@ -12,5 +15,8 @@ interface FlickrApi {
             "&nojsoncallback=1" +
             "&extras=url_s")
     fun fetchPhotos(): Call<FlickrResponse>
+
+    @GET
+    fun fetchUrlBytes(@Url url:String): Call<ResponseBody>
 
 }
