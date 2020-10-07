db7772fcc671fcb36c096ce7bafb8fe9e9b390e8
rjindoo <platypus2k1@gmail.com>
2020-10-07 16:37:22 -0700

    SearchView now remembers the previous search result across onDestroy()

--- app/src/main/java/ryan/d/photogallery/QueryPreferences.kt
+++ app/src/main/java/ryan/d/photogallery/QueryPreferences.kt
@@ -2,8 +2,10 @@ package ryan.d.photogallery
 
 import android.content.Context
 import android.preference.PreferenceManager
+import androidx.core.content.edit
 
 private const val PREF_SEARCH_QUERY = "searchQuery"
+private const val PREF_LAST_RESULT_ID = "lastResultId"
 
 object QueryPreferences {
     fun getStoredQuery(context: Context): String {
@@ -16,5 +18,16 @@ object QueryPreferences {
             .edit()
             .putString(PREF_SEARCH_QUERY, query)
             .apply()
+    }
+
+    fun getLastResultId(context: Context): String {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+            .getString(PREF_LAST_RESULT_ID, "")!!
+    }
+
+    fun setLastResultId(context: Context, lastResultId: String) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .edit()
+            .putString(PREF_LAST_RESULT_ID, lastResultId)
     }
 }
