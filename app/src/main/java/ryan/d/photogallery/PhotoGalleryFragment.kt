ec737bee1f666a66946514b76a46696646fdf736
rjindoo <platypus2k1@gmail.com>
2020-10-06 23:12:25 -0700

    Updated SearchView import to androidx.appcompat.widget.SearchView

--- app/src/main/java/ryan/d/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/ryan/d/photogallery/PhotoGalleryFragment.kt
@@ -8,7 +8,7 @@ import android.os.Handler
 import android.util.Log
 import android.view.*
 import android.widget.ImageView
-import android.widget.SearchView
+import androidx.appcompat.widget.SearchView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
