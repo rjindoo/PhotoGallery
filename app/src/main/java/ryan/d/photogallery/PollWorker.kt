3c5a6386dda27002911c2ad550008c4269aeb50b
rjindoo <platypus2k1@gmail.com>
2020-10-11 15:45:54 -0700

    Created a broadcastReceiver to notify user of photogallery updates

--- app/src/main/java/ryan/d/photogallery/PollWorker.kt
+++ app/src/main/java/ryan/d/photogallery/PollWorker.kt
@@ -2,6 +2,7 @@ package ryan.d.photogallery
 
 import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
@@ -61,8 +62,15 @@ class PollWorker(val context: Context, workerParams: WorkerParameters):
 
             val notificationManager = NotificationManagerCompat.from(context)
             notificationManager.notify(0, notification)
+            context.sendBroadcast(Intent(ACTION_SHOW_NOTIFICATION), PERM_PRIVATE)
         }
-
         return Result.success()
     }
+
+    companion object {
+        const val ACTION_SHOW_NOTIFICATION =
+            "ryan.d.photogallery.SHOW_NOTIFICATION"
+        const val PERM_PRIVATE = "ryan.d.photogallery.PRIVATE"
+    }
+
 }
